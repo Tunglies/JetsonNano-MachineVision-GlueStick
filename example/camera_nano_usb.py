115df8ec079728fd9036261e32b23d78140c0484
Tunglies <tunglies@outlook.com>
2024-03-25 18:51:36 +0800

    remove some example

--- example/camera_nano_usb.py
+++ example/camera_nano_usb.py
@@ -1,17 +1,34 @@
-import cv2
-import nanocamera as nano
-# Create the Camera instance for No rotation (flip=0) with size of 640 by 480
-camera = nano.Camera(camera_type=1, device_id=1, width=640, height=480, fps=30)
+"""
+Using the NanoCamera with USB Cameras
+@author: Ayo Ayibiowu
 
-while camera.isReady():
-    try:
-        frame = camera.read()
-        cv2.imshow("Video Frame", frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+"""
+
+import cv2
+
+# from nanocamera.NanoCam import Camera
+import nanocamera as nano
+
+if __name__ == '__main__':
+    # you can see connected USB cameras by running : ls /dev/video* on the terminal
+    # for usb camera /dev/video2, the device_id will be 2
+
+    # Create the Camera instance
+    camera = nano.Camera(camera_type=1, device_id=1, width=640, height=480, fps=30)
+    print('USB Camera is now ready')
+    while True:
+        try:
+            # read the camera image
+            frame = camera.read()
+            # display the frame
+            cv2.imshow("Video Frame", frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        except KeyboardInterrupt:
             break
-    except KeyboardInterrupt:
-            break
-    
+
+    # close the camera instance
     camera.release()
 
+    # remove camera object
     del camera
--- test.py
+++
@@ -1,4 +0,0 @@
-from src import camera
-with camera.Camera() as video:
-    for frame in video.stream():
-        video.imshow(frame)
